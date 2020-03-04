d7a0f5875a5360c7b6c697b1cfd3a3cbff4c95a8
Alean <alean.asa@hotmail.com>
2020-03-04 23:34:02 +0000

    Finish MovimentServiceTest

--- src/test/java/com/aalmeida/myfinances/service/movimentServiceTest.java
+++ src/test/java/com/aalmeida/myfinances/service/movimentServiceTest.java
@@ -1,11 +1,13 @@
 package com.aalmeida.myfinances.service;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import com.aalmeida.myfinances.exceptions.BusinessRuleException;
 import com.aalmeida.myfinances.model.entity.Moviment;
+import com.aalmeida.myfinances.model.entity.User;
 import com.aalmeida.myfinances.model.enums.MovimentStatus;
 import com.aalmeida.myfinances.model.repository.MovimentRepository;
 import com.aalmeida.myfinances.model.repository.MovimentRepositoryTest;
@@ -24,7 +26,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
-public class movimentServiceTest {
+public class MovimentServiceTest {
 
     @SpyBean
     MovimentServiceImpl service;
@@ -179,4 +181,66 @@ public class movimentServiceTest {
         Assertions.assertThat(result.isPresent()).isFalse();
     }
 
+    @Test
+    public void shouldThrowErrorWhenValidateAMoviment(){
+        Moviment moviment = new Moviment();
+
+        Throwable error = Assertions.catchThrowable(()-> service.validate(moviment));
+        Assertions.assertThat(error).isInstanceOf(BusinessRuleException.class).hasMessage("Invalid description");
+
+        moviment.setDescription("");
+
+        error = Assertions.catchThrowable(() -> service.validate(moviment));
+        Assertions.assertThat(error).isInstanceOf(BusinessRuleException.class).hasMessage("Invalid description");
+        
+        moviment.setDescription("Salary");
+        
+        error = Assertions.catchThrowable(() -> service.validate(moviment));
+        Assertions.assertThat(error).isInstanceOf(BusinessRuleException.class).hasMessage("Invalid month");
+
+        moviment.setYear(0);
+
+        error = Assertions.catchThrowable( () -> service.validate(moviment) );
+		Assertions.assertThat(error).isInstanceOf(BusinessRuleException.class).hasMessage("Invalid month");
+		
+		moviment.setYear(13);
+		
+		error = Assertions.catchThrowable( () -> service.validate(moviment) );
+		Assertions.assertThat(error).isInstanceOf(BusinessRuleException.class).hasMessage("Invalid month");
+		
+		moviment.setMonth(1);
+		
+		error = Assertions.catchThrowable( () -> service.validate(moviment) );
+		Assertions.assertThat(error).isInstanceOf(BusinessRuleException.class).hasMessage("Invalid year");
+		
+		moviment.setYear(202);
+		
+		error = Assertions.catchThrowable( () -> service.validate(moviment) );
+		Assertions.assertThat(error).isInstanceOf(BusinessRuleException.class).hasMessage("Invalid year");
+		
+		moviment.setYear(2020);
+		
+		error = Assertions.catchThrowable( () -> service.validate(moviment) );
+		Assertions.assertThat(error).isInstanceOf(BusinessRuleException.class).hasMessage("Invalid user");
+		
+		moviment.setUser(new User());
+		
+		error = Assertions.catchThrowable( () -> service.validate(moviment) );
+		Assertions.assertThat(error).isInstanceOf(BusinessRuleException.class).hasMessage("Invalid user");
+		
+		moviment.getUser().setId(1l);
+		
+		error = Assertions.catchThrowable( () -> service.validate(moviment) );
+		Assertions.assertThat(error).isInstanceOf(BusinessRuleException.class).hasMessage("Invalid value");
+		
+		moviment.setValue(BigDecimal.ZERO);
+		
+		error = Assertions.catchThrowable( () -> service.validate(moviment) );
+		Assertions.assertThat(error).isInstanceOf(BusinessRuleException.class).hasMessage("Invalid value");
+		
+		moviment.setValue(BigDecimal.valueOf(1));
+		
+		error = Assertions.catchThrowable( () -> service.validate(moviment) );
+		Assertions.assertThat(error).isInstanceOf(BusinessRuleException.class).hasMessage("Invalid type");
+    }
 }
